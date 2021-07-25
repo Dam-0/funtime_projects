d4b8265cfa163bebbd57528c0ea2581382d46e52
Damien Peters <damien.peters2000@gmail.com>
2021-07-25 18:10:36 +0800

    update

--- 2021/rust_stuff/starter_programs/src/calculator.rs
+++ 2021/rust_stuff/starter_programs/src/calculator.rs
@@ -1,5 +1,4 @@
 use std::io;
-use std::{i32};
 
 mod util;
 
@@ -21,15 +20,15 @@ fn main() -> io::Result<()> {
         let user_second_num = input_int("Enter second Number: ")?;
 
         // Converting string to integer
-        let aint: u32 = user_first_num.trim().parse().ok().expect("test");
+        let first_num: u32 = user_first_num.trim().parse().ok().expect("test");
 
-        let bint: u32 = user_second_num.trim().parse().ok().expect("test");
+        let second_num: u32 = user_second_num.trim().parse().ok().expect("test");
 
         match user_choice.as_str() {
-            "1" => println!("sum is: {}", aint + bint),
-            "2" => println!("difference is: {}", aint - bint),
-            "3" => println!("Multiply is: {}", aint * bint),
-            "4" => println!("division is: {}", aint / bint),
+            "1" => println!("sum is: {}", first_num + second_num),
+            "2" => println!("difference is: {}", first_num - second_num),
+            "3" => println!("Multiply is: {}", first_num * second_num),
+            "4" => println!("division is: {}", first_num / second_num),
             "5" => return Ok(()),
             _ => println!("Choose something valid")
         }

b8d96f91acfb185a32b6f178aeda40874135ff03
Damien Peters <damien.peters2000@gmail.com>
2021-07-25 13:24:45 +0800

    upadtes

---
+++ 2021/rust_stuff/starter_programs/src/file_syntax.rs
@@ -0,0 +1,66 @@
+use std::io;
+
+fn main() -> io::Result<()> {
+    println!("Hello, world!");
+
+    let number = 5;
+    match number {
+        0 => println!("Zero"),
+        2 => println!("Two"),
+
+        // {} substitute first varible
+        i => println!("Got: {}", i)
+    }
+
+    let fruit ="apples";
+
+    if fruit == "apples" {
+        println!("Apples is correct")
+    }
+
+    let list = [1, 2, 3];
+
+    // & referance to fix sized list / pointer
+    let list = &[1, 2, 3];
+
+    // mutable, can add items
+    let mut list = vec![1, 2, 3];
+    list.push(4);
+
+    println!("List has: {:?}", list);
+
+
+    let string_referance = "Cannot change";
+
+    // mutable
+    let mut string = "apples".to_string();
+    string.push_str( " and oranges");
+    println!("we have {}", &string);
+
+    print_hello("Damo");
+    print_hello("Aido");
+
+    let name = input( "What is your name?")?;
+
+    // remove end ; to make it return statement
+    println!("Hello, '{}'", name);
+
+    // return statement
+    Ok(())  
+}
+
+// &str , I dont own it. Just borrowing for this func
+fn print_hello(name: &str) {
+    println!("Hello {}!", name)
+}
+
+/// # Input from user
+/// Get some strings from the user
+fn input(prompt: &str) -> io::Result<String> {
+    println!("{}", prompt);
+    let mut input_value = String::new();
+    io::stdin() //inefficant. Should be using one Stdin handle 
+        .read_line(&mut input_value)?;
+    Ok(input_value.trim().to_string())
+}
+

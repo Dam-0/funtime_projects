efd2d3b69476578abf673c6382840fb2582bf6c7
Damien Peters <damien.peters2000@gmail.com>
2021-07-29 22:24:25 +0800

    updates

--- 2021/bash_scripts/damo_fetch.sh
+++ 2021/bash_scripts/damo_fetch.sh
@@ -2,9 +2,8 @@
 
 echo "OS: $(lsb_release -sd)"
 echo "Kernel: $(uname -r)"
-echo "Packages: $(dpkg-query -f '.\n' -W | wc -l)(pkg)"
+echo "Packages: $(dpkg-query -f '.\n' -W | wc -l) (pkg)"
 echo "Shell: $SHELL"
-echo "Terminal: "
-echo "Cpu: "
-echo "Gpu: "
+echo "CPU: $(lscpu | grep 'Model name' | cut -f 2 -d ":" | awk '{$1=$1}1')"
+echo "GPU: $(lshw -C display  2>/dev/null | grep 'product' | cut -f 2 -d ":") $(lshw -C display  2>/dev/null | grep 'bus info' | cut -f 2 -d ":")"
 echo "Mem: "

42d3c8955388994ccdfcaec5da93d69084cef2ca
Dam-0 <Damien.peters2000@gmail.com>
2021-08-09 21:07:59 -0400

    updates

--- 2021/bash_scripts/scan_tool.sh
+++ 2021/bash_scripts/scan_tool.sh
@@ -8,10 +8,6 @@ error_check() {
 		echo "Usage: scan_tool <ip>"
 		exit 1
 
-	#elif [ "$EUID" -ne 0 ]; then
-	#	echo "Script must be run as root"
-	#	exit 1
-
 	#Checks if user input vaild IP Address
 	elif [[ "$ip" =~ ^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$ ]]; then
 		return 0
@@ -27,21 +23,19 @@ fping -c1 -t300 $ip 2>/dev/null 1>/dev/null
 
 #Checks if the Host is up
 	if [ "$?" = 0 ]; then
+		nmap -sV "$ip" | grep -E "([0-9]{1,5})\/(tcp|udp)" > /tmp/services
 		return 0
-
 	else
 		echo "Failed to find host"
 		exit 1
-	fi 
-
-nmap -sV "$ip" | grep -E "([0-9]{1,5})\/(tcp|udp)" > /tmp/services.json
+	fi
 }
 
 #Checks services against search sploit
 search_sploit(){
 
 	#Checks if services are found
-	echo `wc -l /tmp/services.json | grep -o ^[1-9]` | read service_total
+	echo $(wc -l /tmp/services | grep -o ^[1-9]) | read service_total
 	if [ "$service_total" == 0 ]; then
 		echo "No Services found on host"
 		$service_total
@@ -49,28 +43,49 @@ search_sploit(){
 	fi
 
 	#checks json is correct format
-	grep -q -E "([0-9]{1,5})\/(tcp|udp)" /tmp/services.json
+	grep -q -E "([0-9]{1,5})\/(tcp|udp)" /tmp/services
 	if [[ $? != 0 ]]; then
 		echo "Error has occured in parsing the JSON file"
 		exit 1
 	fi
 
 	#Gets the services and versions
-	awk '{print $4" "$5}' < /tmp/services.json > /tmp/output
+	awk '{print $4" "$5}' < /tmp/services > /tmp/output
 
-	echo "=======================" > exploits.txt
-	echo "== Possible Exploits ==" >> exploits.txt
-	echo "=======================" >> exploits.txt
-	while IFS='' read -r LINE || [ -n "${LINE}" ]; do
-   		searchsploit ${LINE} | sed -e '1,3d' | head -n -2 >> exploits.txt
+	echo "==============================" > exploits.txt
+	echo "== Possible Vulnerabilities ==" >> exploits.txt
+	echo "==============================" >> exploits.txt
+
+	while IFS='' read LINE || [[ -n "${LINE}" ]]; do
+   		searchsploit ${LINE} | sed -e '1,2d' | head -n -2 >> exploits.txt
    		echo "==" >> exploits.txt
 	done < /tmp/output
 
 }
 
+#Adds loading animation
+spinner()
+{
+    local pid=$!
+    local delay=0.75
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+
 error_check
-host_scan
-search_sploit
+host_scan &
+spinner
+search_sploit &
+spinner
+
 
 cat exploits.txt
 

8af016b10773c0987f5e36fb44e7cb4a053bf2e7
Dam-0 <Damien.peters2000@gmail.com>
2021-08-11 04:25:49 -0400

    fuck jq

--- 2021/bash_scripts/json_query.sh
+++ 2021/bash_scripts/json_query.sh
@@ -7,7 +7,6 @@ readarray -t my_array < <(jq -c '.[]' $file)
 
 let num=0
 rm /tmp/test1 > /dev/null 2>&1
-rm /tmp/test2 > /dev/null 2>&1
 
 for i in "${my_array[@]}"; do 
     let num2=0
@@ -16,7 +15,7 @@ for i in "${my_array[@]}"; do
     echo $index_num
     until [[ $num2 == $index_num ]]; do
         jq -r ".[$num].RESULTS_EXPLOIT[$num2].Title" $file >> /tmp/test1
-        jq -r ".[$num].RESULTS_EXPLOIT[$num2].Path" $file >> /tmp/test2
+        jq -r ".[$num].RESULTS_EXPLOIT[$num2].Path" $file >> /tmp/test1
         num2=$(($num2+1))
     done
     num=$(($num+1)) 

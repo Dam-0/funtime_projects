f55daecd1ecf4b9460c62ceefe7df5b548a1c2f6
Dam-0 <Damien.peters2000@gmail.com>
2021-08-11 04:24:24 -0400

    removed redundant stuff

--- 2021/bash_scripts/json_query.sh
+++ 2021/bash_scripts/json_query.sh
@@ -5,16 +5,6 @@ file="results/192.168.92.137.json"
 rm /tmp/test
 readarray -t my_array < <(jq -c '.[]' $file)
 
-for item in "${my_array[@]}";do
-    search_query=$(jq '.SEARCH' <<< "$item")
-    search_results=$(jq '.RESULTS_EXPLOIT[]."Title"' <<< "$item")
-    exploit_location=$(jq '.RESULTS_EXPLOIT[]."Path"' <<< "$item")
-    echo "Search Query:" $search_query >> /tmp/test
-    echo "Results: $search_results" >> /tmp/test
-    echo "Exploit_location: $exploit_location" >> /tmp/test
-
-done
-
 let num=0
 rm /tmp/test1 > /dev/null 2>&1
 rm /tmp/test2 > /dev/null 2>&1

590ad3797d45fe73de0a49dba90b32837b5f2ae9
Dam-0 <Damien.peters2000@gmail.com>
2021-08-11 02:36:09 -0400

    fixed paths

--- 2021/bash_scripts/json_query.sh
+++ 2021/bash_scripts/json_query.sh
@@ -8,7 +8,7 @@ readarray -t my_array < <(jq -c '.[]' $file)
 for item in "${my_array[@]}";do
     search_query=$(jq '.SEARCH' <<< "$item")
     search_results=$(jq '.RESULTS_EXPLOIT[]."Title"' <<< "$item")
-    exploit_location$(jq '.RESULTS_EXPLOIT[]."Path"' <<< "$item")
+    exploit_location=$(jq '.RESULTS_EXPLOIT[]."Path"' <<< "$item")
     echo "Search Query:" $search_query >> /tmp/test
     echo "Results: $search_results    Exploit_location: $exploit_location /n" >> /tmp/test
 

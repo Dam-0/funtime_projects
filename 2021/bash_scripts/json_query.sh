fa8bab806052da3d2e1fb6d4b4a4be6c478aeefd
Dam-0 <Damien.peters2000@gmail.com>
2021-08-11 04:23:10 -0400

    Know knows num of index in select array

--- 2021/bash_scripts/json_query.sh
+++ 2021/bash_scripts/json_query.sh
@@ -16,21 +16,18 @@ for item in "${my_array[@]}";do
 done
 
 let num=0
-rm test1 > /dev/null 2>&1
-rm test2 > /dev/null 2>&1
+rm /tmp/test1 > /dev/null 2>&1
+rm /tmp/test2 > /dev/null 2>&1
 
-#for i in "${my_array[@]}"; do 
-#    let num2=0
-#    until [[ $num2 == $(jq -r ".[$num].RESULTS_EXPLOIT[] | length" $file) ]]; do
-#        jq -r ".[$num].RESULTS_EXPLOIT[$num2].Title" $file >> test1
-#        jq -r ".[$num].RESULTS_EXPLOIT[$num2].Path" $file >> test2
-#        echo $num2
-#        num2=$(($num2+1))
-#    done
-#    num=$(($num+1)) 
-#done
-
-#jq -r ".[0].RESULTS_EXPLOIT[]" $file | sed 's/[^{]//g' | awk '{ print length }'
-
-jq -r ".[1].RESULTS_EXPLOIT" $file > /tmp/fuck
-awk -F\{ '{print NF-1}' /tmp/fuck
+for i in "${my_array[@]}"; do 
+    let num2=0
+    jq -r ".[$num].RESULTS_EXPLOIT[]" $file | sed 's/[^{]//g' | awk '{ print length }' | sed '/^0/d' > /tmp/fuck
+    index_num=$(wc -l /tmp/fuck | awk '{print $1}' )
+    echo $index_num
+    until [[ $num2 == $index_num ]]; do
+        jq -r ".[$num].RESULTS_EXPLOIT[$num2].Title" $file >> /tmp/test1
+        jq -r ".[$num].RESULTS_EXPLOIT[$num2].Path" $file >> /tmp/test2
+        num2=$(($num2+1))
+    done
+    num=$(($num+1)) 
+done

2a56aae0c768eec4c2f4f5736bfbc61ad2ba0174
Dam-0 <Damien.peters2000@gmail.com>
2021-08-13 03:42:49 -0400

    did some stuff

--- 2021/rust_stuff/damo_fetch/src/main.rs
+++ 2021/rust_stuff/damo_fetch/src/main.rs
@@ -1,61 +1,71 @@
-use nixinfo::{
-    cpu, device, distro, environment, gpu, hostname, kernel, memory, packages,
-    terminal, uptime};
+use nixinfo::{cpu, device, distro, environment, gpu, hostname, kernel, memory, packages, terminal, uptime};
+use std::thread;
 
 
 fn cpu_details() {
-    println!("cpu name: {}", nixinfo::cpu().unwrap())
+    println!("cpu name: {}", cpu().unwrap())
     //println!("cpu temp: {}", nixinfo::temp().unwrap())
 
 }
 
 fn device_details() {
-    println!("machine: {}", nixinfo::device().unwrap())
+    println!("machine: {}", device().unwrap())
 }
 
 fn distro_details() {
-    println!("distro: {}", nixinfo::distro().unwrap())
+    println!("distro: {}", distro().unwrap())
 }
 
 fn gpu_details() {
-    println!("gpu: {}", nixinfo::gpu().unwrap())
+    println!("gpu: {}", gpu().unwrap())
 }
 
 fn host_details() {
-    println!("hostname: {}", nixinfo::hostname().unwrap())
+    println!("hostname: {}", hostname().unwrap())
 }
 
 fn memory_details() {
-    println!("mem: {}", nixinfo::memory().unwrap())
+    println!("mem: {}", memory().unwrap())
 }
 
 fn package_details() {
-    println!("packages No: {}", nixinfo::packages("apt").unwrap())
+    println!("packages No: {}", packages("apt").unwrap())
 }
 
 fn terminal_details() {
-    println!("terminal: {}", nixinfo::terminal().unwrap())
+    println!("terminal: {}", terminal().unwrap())
 }
 
 fn uptime_details() {
-    println!("uptime: {}", nixinfo::uptime().unwrap())
+    println!("uptime: {}", uptime().unwrap())
 }
 
 fn environment_details() {
-    println!("de: {}", nixinfo::environment().unwrap())
+    println!("de: {}", environment().unwrap())
 }
+
+fn kernel_details() {
+    println!("kernal: {}", kernel().unwrap())
+}
+
+
+
+
+
+
 
 fn main() {
 
+    thread::spawn(|| {package_details()});
     cpu_details();
     device_details();
     distro_details();
     gpu_details();
     host_details();
     memory_details();
-    package_details();
     terminal_details();
     uptime_details();
     environment_details();
+    kernel_details();
     
 }

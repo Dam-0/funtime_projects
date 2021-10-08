0301dc7e8baa98a6d35574e598c81b3eacd49696
Damo <damien.peters2000@gmail.com>
2021-10-08 04:16:36 -0400

    hmmhmm

--- 2021/Perl_scripts/guess_game.pl
+++ 2021/Perl_scripts/guess_game.pl
@@ -7,22 +7,25 @@ my $highest_age = 100;
 my $guess = 1;
 
 my $rand_age = int(rand($highest_age-$lowest_age)) + $lowest_age;
-print $rand_age, "\n";
+print ($rand_age, "\n");
+print ($guess, "\n"):
 
-
-while ( $guess == 0 ) {
+while( $guess != 1 ) {
     print "Input Number: ";
     chomp( my $num_guess = <STDIN>);
 
-    if ($num_guess < $rand_age) {
+    if($num_guess < $rand_age) {
         print ("q1 \n");
     }
-    elsif ($num_guess > $rand_age) {
+    elsif($num_guess > $rand_age) {
         print ("q2 \n");
     }
-    elsif ($num_guess == $rand_age) {
+    elsif($num_guess == $rand_age) {
         print ("q3 \n");
         my $guess = 0;
+    }
+    else {
+        print ("WHOOPS \n");
     }
 
 }

2665c2cb4b78c66db180f792b06a6d2bddf8e4e8
Damo <damien.peters2000@gmail.com>
2021-11-07 22:41:23 -0500

    new stuff

--- 2021/Perl_scripts/guess_game.pl
+++ 2021/Perl_scripts/guess_game.pl
@@ -4,28 +4,26 @@ use strict;
 
 my $lowest_age = 10;
 my $highest_age = 100;
-my $guess = 1;
+my $guess = 0;
 
 my $rand_age = int(rand($highest_age-$lowest_age)) + $lowest_age;
-print ($rand_age, "\n");
-print ($guess, "\n"):
 
 while( $guess != 1 ) {
     print "Input Number: ";
     chomp( my $num_guess = <STDIN>);
 
     if($num_guess < $rand_age) {
-        print ("q1 \n");
+        print ("Age is higher. \n");
     }
     elsif($num_guess > $rand_age) {
-        print ("q2 \n");
+        print ("Age is lower. \n");
     }
     elsif($num_guess == $rand_age) {
-        print ("q3 \n");
-        my $guess = 0;
+        print ("You got it. \n");
+        my $guess = 1;
     }
     else {
         print ("WHOOPS \n");
     }
-
 }
+exit 0
